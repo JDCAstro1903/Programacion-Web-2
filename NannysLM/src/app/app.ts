02842690d98888d405af97771f4581b7ad8be97c
JDCAstro1903 <133943864+JDCAstro1903@users.noreply.github.com>
2025-09-18 21:22:56 -0700

    LandingPage
    Le estoy enseñando quien es la perra a quien, voy perdiendo

--- NannysLM/src/app/app.ts
+++ NannysLM/src/app/app.ts
@@ -1,12 +1,16 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './components/header/header';
+import { HeroComponent } from './components/hero/hero';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, HeaderComponent, HeroComponent],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrls: ['./app.css']
 })
-export class App {
-  protected readonly title = signal('NannysLM');
+
+export class AppComponent {
+  title = 'NannysLM';
 }
