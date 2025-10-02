b7e7ba365387dcf56fa44050393cd740c684e141
JDCAstro1903 <133943864+JDCAstro1903@users.noreply.github.com>
2025-10-01 17:09:19 -0700

    Registro, login y mejoras en el diseño del landing page
    Falta la funcionalidad que se la vamos a dar con python y ya empezamos a crear el menu del administrador

--- NannysLM/src/app/app.ts
+++ NannysLM/src/app/app.ts
@@ -1,12 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from './components/header/header';
-import { HeroComponent } from './components/hero/hero';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, HeroComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrls: ['./app.css']
 })
